b638fb90dde0d004280d7d8b80e24d4bcdad6237
caroline-chaim <caroline.chaim@gmail.com>
2026-06-07 08:22:05 -0300

    mudando icone e nome

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Demo",
+      title: "HexaTracker",
       theme: ThemeData(
         primaryColor: Colors.amber,
       ),
